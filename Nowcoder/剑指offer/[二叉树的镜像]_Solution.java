1d75aba46ec358fafaff127d50a9e48dfa9dab1e
ShijunDeng <M201572621@hust.edu.cn>
2017-03-20 15:32:35 +0800

    二叉树的镜像
    二叉树的镜像
    时间限制：1秒 空间限制：32768K 热度指数：13546
    算法知识视频讲解
    题目描述
    操作给定的二叉树，将其变换为源二叉树的镜像。
    输入描述:
    二叉树的镜像定义：源二叉树

---
+++ Nowcoder/剑指offer/[二叉树的镜像]_Solution.java
@@ -0,0 +1,44 @@
+/**二叉树的镜像
+时间限制：1秒 空间限制：32768K 热度指数：13546
+ 算法知识视频讲解
+题目描述
+操作给定的二叉树，将其变换为源二叉树的镜像。 
+输入描述:
+二叉树的镜像定义：源二叉树 
+    	    8
+    	   /  \
+    	  6   10
+    	 / \  / \
+    	5  7 9 11
+    	镜像二叉树
+    	    8
+    	   /  \
+    	  10   6
+    	 / \  / \
+         
+
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root == null){
+            return;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        Mirror(root.left);
+		Mirror(root.right);
+    }
+}         
+         
