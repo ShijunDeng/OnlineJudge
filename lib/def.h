a1c49f0edec8c34377735a9e8ddd4fbc4ffdc163
ShijunDeng <M201572621@hust.edu.cn>
2017-04-27 09:18:07 +0800

    def.h

--- lib/def.h
+++ lib/def.h
@@ -1,3 +1,3 @@
 
-const int MAXINT =((unsigned int)-1)>>1;
-const int MININT= ~ (((unsigned int)-1)>>1);
+const int INT_MAX = ((unsigned int) - 1) >> 1;
+const int INT_MIN = ~(((unsigned int) - 1) >> 1);
