e5525ee86dc9de8de9030b2c076d54fbad535e7a
ShijunDeng <M201572621@hust.edu.cn>
2017-04-27 10:04:22 +0800

    qsort使用

--- lib/qsort使用.c
+++ lib/qsort使用.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#include<malloc.h>
 //排序函数头文件
 #include<stdlib.h>
 
@@ -7,18 +8,58 @@ int comp1(const void* a, const void* b)
 {
     return *((int*)a) - *((int*)b);
 }
-int comp2(const void* a, const void* b)
+//普通的二维数组
+int comp2(const void *a, const void *b)
 {
-    return ((int*)a)[1] - ((int*)b)[1] ;
+    return ((int *)a)[0] - ((int *)b)[0];
+}
+//malloc申请的二维数组
+int comp3(const void* a, const void* b)
+{
+    int *ap = *(int **)a;
+    int *bp = *(int **)b;
+    return ap[0] - bp[0];
+}
+//malloc申请的二维数组
+int comp4(const void* a, const void* b)
+{
+    int *ap = *(int **)a;
+    int *bp = *(int **)b;
+
+    if (ap[0] == bp[0])
+    {
+        return ap[1] - bp[1];
+    }
+    else
+    {
+        return ap[0] - bp[0];
+    }
 }
 int main()
 {
     int data[][2] = {{1, 6}, {4, 7}, {2, 11}, {3, 1}, {17, 32}, {13, 16}, {21, 23}, {48, 14}, {16, 66}, {27, 15}};
     int i = 0;
-    qsort(data, 10, sizeof(int[2]), comp2);
+    int *b, **a;
+    a = (int**)malloc(10 * sizeof(int*)); //这里是对int*来分配。
     for (i = 0; i < 10; i++)
     {
-        printf("%d %d\n", data[i][0], data[i][1]);
+        a[i] = malloc(2 * sizeof(int));
+        a[i][0] = data[i][0];
+        a[i][1] = data[i][1];
+    }
+    qsort(a, 10, sizeof(a[0]), comp3);
+    for (i = 0; i < 10; i++)
+    {
+        printf("a %d %d\n", a[i][0], a[i][1]);
+    }
+    qsort(data, 10, sizeof(data[0]), comp2);
+    for (i = 0; i < 10; i++)
+    {
+        printf("data %d %d\n", data[i][0], data[i][1]);
+    }
+    for (i = 0; i < 10; i++)
+    {
+        free(a[i]);
     }
     return 0;
 }
