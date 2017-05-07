117172f16c68c59c1ae7822b5d6fe4f5a3b2a796
ShijunDeng <M201572621@hust.edu.cn>
2017-05-07 16:52:02 +0800

    113. Path Sum II

--- Leetcode/2017/PathSumII113.java
+++ Leetcode/2017/PathSumII113.java
@@ -38,20 +38,45 @@ public class Solution {
         if (root == null) {
             return rs;
         }
-        searchPathSum(root, sum, 0, new ArrayList<Integer>(), rs); 
+        //searchPathSumRecur(root, sum, 0, new ArrayList<Integer>(), rs); 
+        
+        TreeNode p = root;
+        Stack<TreeNode> S = new Stack<TreeNode>();
+        int curSum = 0;
+        Map<TreeNode,List<Integer>> map1 = new HashMap<TreeNode,List<Integer>>();
+        Map<TreeNode,Integer> map2 = new HashMap<TreeNode,Integer>();
+        List<Integer> tmp = new ArrayList<Integer>();
+        while (S.isEmpty() == false || p != null) {
+            if (p != null) {
+                curSum += p.val;
+                S.push(p);
+                tmp.add(p.val);
+                map1.put(p, new ArrayList<Integer>(tmp));
+                map2.put(p, curSum);
+                if (p.left == null && p.right == null && curSum == sum){
+                    rs.add(map1.get(p));
+                } 
+                p = p.left;
+            } else{
+                p = S.pop();
+                tmp = map1.get(p);
+                curSum = map2.get(p);
+                p = p.right;
+            }
+        }
         return rs;
     }
     
-    public void searchPathSum(TreeNode root, int sum, int curSum, List<Integer> cur, List<List<Integer>> rs) {
+    public void searchPathSumRecur(TreeNode root, int sum, int curSum, List<Integer> cur, List<List<Integer>> rs) {
         cur.add(root.val);
         if (root.left == null && root.right == null && curSum + root.val == sum){
             rs.add(new ArrayList<Integer>(cur));
         } else {
             if(root.left != null){
-                searchPathSum(root.left, sum, curSum + root.val, new ArrayList<Integer>(cur), rs); 
+                searchPathSumRecur(root.left, sum, curSum + root.val, new ArrayList<Integer>(cur), rs); 
             } 
             if(root.right != null){
-                searchPathSum(root.right, sum, curSum + root.val, new ArrayList<Integer>(cur), rs); 
+                searchPathSumRecur(root.right, sum, curSum + root.val, new ArrayList<Integer>(cur), rs); 
             }
         }
     }
